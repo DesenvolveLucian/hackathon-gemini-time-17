ea56a83bb871d478de478485a259f6e2abac3d62
Julia Ingrid Santos Alencar <juliaingrid.dev@gmail.com>
2022-09-24 16:40:10 -0300

    fix: corrige requisição do getRestaurantes feat: add func getDetalhesRestaurantes

--- src/services/restaurantes.service.js
+++ src/services/restaurantes.service.js
@@ -2,9 +2,19 @@ import Api from "./api";
 
 export async function getRestaurantes(id) {
   try {
-    const restaurantes = await Api.get(`/restaurantes/${id}.json`);
+    const restaurantes = await Api.get(`/restaurantes/:${id}.json`);
     return restaurantes.data;
   } catch (err) {
     throw err;
   }
 };
+
+export async function getDetalheRestaurante(id) {
+  try {
+    const restaurante = await Api.get(`/detalhes/${id}.json`);
+    return restaurante.data;
+  } catch (err) {
+    throw err;
+  }
+};
+0
